81a787678d1a82cb68587f3e95074c4708019768
istroxpro <istrodantir@icloud.com>
2022-05-31 18:29:09 +0700

    MESTO project 7 initial commit

---
+++ scripts/card.js
@@ -0,0 +1,50 @@
+import * as data from "./variables.js";
+import {
+    popupOpen
+} from "./index.js";
+export class Card {
+    constructor(data, cardSelector) {
+        this._title = data.name;
+        this._image = data.link;
+        this._cardSelector = cardSelector;
+    };
+
+    _getTemplate() {
+        const cardsTemplate = document.querySelector(this._cardSelector);
+        const cardsElem = cardsTemplate.content.cloneNode(true);
+        const cardTitle = cardsElem.querySelector('.cards__title');
+        const cardImage = cardsElem.querySelector('.cards__image');
+        cardImage.src = this._image;
+        cardImage.alt = this._title;
+        cardTitle.textContent = this._title;
+
+        return cardsElem;
+    };
+
+    _imagePreview() {
+        popupOpen(data.imagePopup);
+        data.imageView.src = this._image;
+        data.imageText.textContent = this._title;
+    };
+
+    _setEventListeners() {
+        const likeButton = this._element.querySelector('.cards__like-button');
+        const deleteButton = this._element.querySelector('.cards__delete-button');
+        const img = this._element.querySelector('.cards__image')
+        img.addEventListener('click', () => {
+            this._imagePreview()
+        });
+        likeButton.addEventListener('click', () => {
+            likeButton.classList.toggle('cards__like-button_active')
+        });
+        deleteButton.addEventListener('click', () => {
+            deleteButton.closest('.cards__block').remove()
+        });
+    };
+
+    renderCard() {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+        return this._element;
+    };
+};
---
+++ scripts/formValidator.js
@@ -0,0 +1,66 @@
+export class FormValidator {
+    constructor({
+        ...base
+    }, formElement) {
+        this._data = {
+            ...base
+        };
+        this._formElement = formElement;
+        this._inputList = Array.from(this._formElement.querySelectorAll(this._data.inputSelector));
+        this._buttonElement = this._formElement.querySelector(this._data.submitButtonSelector);
+    };
+    // Показываем ошибку
+    _showInputError(inputElement) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.add(this._data.inputErrorClass);
+        errorElement.textContent = inputElement.validationMessage;
+        errorElement.classList.add(this._data.errorClass);
+    };
+    // Скрываем ошибку
+    _hideInputError(inputElement) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        errorElement.textContent = ''
+        inputElement.classList.remove(this._data.inputErrorClass);
+        errorElement.classList.remove(this._data.errorClass);
+    };
+    // Валидация
+    _checkInputValidity(inputElement) {
+        if (!inputElement.validity.valid) {
+            this._showInputError(inputElement)
+        } else {
+            this._hideInputError(inputElement)
+        };
+    };
+    // Проверка на валидность
+    _hasInvalidInput() {
+        return this._inputList.some((inputElement) => {
+            return !inputElement.validity.valid;
+        });
+    };
+    _disableSubmitButton() {
+        this._buttonElement.classList.add(this._data.inactiveButtonClass);
+        this._buttonElement.disabled = "disabled";
+    };
+
+    // Изменение отображения кнопки сабмита
+    toggleButtonState() {
+        if (this._hasInvalidInput()) {
+            this._disableSubmitButton()
+        } else {
+            this._buttonElement.classList.remove(this._data.inactiveButtonClass);
+            this._buttonElement.disabled = "";
+        }
+    };
+    _setEventListeners() {
+        this.toggleButtonState();
+        this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener('input', () => {
+                this._checkInputValidity(inputElement);
+                this.toggleButtonState();
+            });
+        });
+    };
+    enableValidation() {
+        this._setEventListeners();
+    };
+};
